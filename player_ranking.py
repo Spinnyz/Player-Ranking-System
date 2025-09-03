c300043a93f24d295c7ce3bec88131c234163896
Pedro Lucas <pedrolucasgonza31@gmail.com>
2025-09-03 19:41:10 -0300

    up

--- player_ranking.py
+++ player_ranking.py
@@ -1,4 +1,4 @@
-class jogador:
+class Jogador:
     def __init__(self,nome,pontuacao):
         self.nome = nome
         self.pontuacao = pontuacao
@@ -8,3 +8,18 @@ class jogador:
 
     def __str__(self):
         return f"{self.nome}: {self.pontuacao} pontos"
+
+class Ranking:
+    def __init__(self):
+        self.jogadores = []
+
+    def adicionar_jogador(self,nome,pontuacao):
+        self.jogadores.append(Jogador(nome,pontuacao))
+    
+    def remover_jogador(self,nome):
+        for jogador in self.jogadores:
+            if jogador.nome == nome:
+                self.jogadores.remove(jogador)
+                print (f"O jogador {nome} foi removido do ranking.")
+                return
+        print (f"O jogador {nome} não foi encontrado no ranking.")
