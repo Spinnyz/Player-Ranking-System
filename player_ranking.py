c62a5c3ef0f36a1287ef9d95d8f7e7f6a9340eff
Pedro Lucas <pedrolucasgonza31@gmail.com>
2025-09-03 19:23:38 -0300

    up

--- player_ranking.py
+++ player_ranking.py
@@ -2,3 +2,9 @@ class jogador:
     def __init__(self,nome,pontuacao):
         self.nome = nome
         self.pontuacao = pontuacao
+    
+    def adicionar_pontos(self,pontos):
+        self.pontuacao += pontos
+
+    def __str__(self):
+        return f"{self.nome}: {self.pontuacao} pontos"
