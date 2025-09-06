6ed09d720ff999be262cb19c6e70387e84671068
Pedro Lucas <pedrolucasgonza31@gmail.com>
2025-09-05 21:45:23 -0300

    up

--- player_ranking.py
+++ player_ranking.py
@@ -45,4 +45,40 @@ class Ranking:
                 print (jogador)
                 return
         print (f"O jogador {nome} não foi encontrado")
-    
+
+while True:
+    print ("""
+[1] Adicionar jogador
+[2] Remover jogador
+[3] Atualizar pontuação
+[4] Listar ranking
+[5] Top jogadores
+[6] Buscar jogador
+[0] Sair
+""")
+    opcao = input("Escolha uma opção: ")
+
+
+    match opcao:
+        case "1":
+            nome = input("Digite o nome do jogador: ")
+            pontuacao = int(input("Digite a pontuação do jogador: "))  
+            Ranking.adicionar_jogador(nome,pontuacao)
+        case "2":
+            nome = input("Digite o nome do jogador: ")
+            Ranking.remover_jogador(nome)
+        case "3":
+            nome = input("Digite o nome do jogador: ")
+            pontos = int(input("Digite a quantidade de pontos: "))
+            Ranking.atualizar_pontuacao(nome,pontos)
+        case "4":
+            Ranking.listar_ranking()
+        case "5":
+            print (Ranking.top_jogadores())
+        case "6":
+            nome = input("Digite o nome do jogador: ")
+            Ranking.buscar_jogadores(nome)
+        case "0":
+            break
+        case _:
+            print ("Opção inválida")
